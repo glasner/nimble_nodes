d5ca788a63ffa798bab791e06a0e8b3233bc7465
Jordan Glasner <jordan@digitalignition.com>
2010-03-11 22:41:02 -0500

    use new NimbleNodes.monitor method

--- lib/nimble_nodes/middleware.rb
+++ lib/nimble_nodes/middleware.rb
@@ -6,8 +6,7 @@ module NimbleNodes
 
 
     def call(env)
-      @report = NimbleNodes::Report.new(env)
-      @report.post if @report.post?
+      NimbleNodes.monitor(env)
       # now, execute the request using our Rails app
       response = @app.call(env)
     end
