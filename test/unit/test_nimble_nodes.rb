55d53c202213ee41486a715b94029941edeafbb0
Jordan Glasner <jordan@digitalignition.com>
2010-04-06 11:28:32 -0400

    moved tests to App

--- test/unit/test_nimble_nodes.rb
+++ test/unit/test_nimble_nodes.rb
@@ -6,46 +6,11 @@ class TestNimbleNodes < Test::Unit::TestCase
   context "NimbleNodes module" do
     setup { @module = NimbleNodes }
     
-    context "in a Rails app" do
-      
-      setup { NimbleNodes::App.stubs(:rails?).returns(true) }
-
-      context "pre 2.3" do
-        setup { NimbleNodes::App.stubs(:rails_version).returns(2.2) }
-
-        should "use NimbleNodes::Filter as reporter" do
-          assert_equal 'filter', @module.reporter.split('/').last
-        end
-
-      end 
-      
-      context "2.3 or later" do
-        setup { NimbleNodes::App.stubs(:rails_version).returns(2.3) }
-
-        should "use NimbleNodes::Middleware as reporter" do
-          assert_equal 'middleware', @module.reporter.split('/').last
-        end
-
-      end   
-
-    end
-    
-    context "in a non-Rails app" do
-      setup { NimbleNodes::App.stubs(:rails?).returns(false) }
-      
-      should "use NimbleNodes::Middleware" do
-        assert_equal 'middleware8', @module.reporter.split('/').last
-      end
-      
-    end
 
     context "App installed at NimbleNodes" do
 
       setup { 
-        NimbleNodes::App.stubs({
-          :name => 'nimble-nodes-test',
-          :token => 'token'
-        })
+        setup_installed_app
       } 
 
       context "not paused" do
