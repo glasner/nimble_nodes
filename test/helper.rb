f10333ad77f2b2a9c9569428034272e4759bccf0
Jordan Glasner <jordan@digitalignition.com>
2010-04-06 11:28:15 -0400

    setup helpers to stub valid and invalid apps

--- test/helper.rb
+++ test/helper.rb
@@ -8,4 +8,19 @@ require 'fakeweb'
 require File.expand_path(File.dirname(__FILE__)) + '/../lib/nimble_nodes'
 
 class Test::Unit::TestCase
+  
+  def setup_installed_app
+    NimbleNodes::App.stubs({
+      :name => "nimble-nodes-test", 
+      :token => 'test'
+    })
+  end
+  
+  def setup_invalid_app
+    NimbleNodes::App.stubs({
+      :name => nil, 
+      :token => nil
+    })
+  end
+  
 end
